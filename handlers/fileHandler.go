a3d389d3abdd4aa103d34fab2ad56bf4bb6f6ca4
MelleNooijen <mellenooijen@gmail.com>
2022-07-25 23:18:23 +0200

    File serving done

--- handlers/fileHandler.go
+++ handlers/fileHandler.go
@@ -1,7 +1,18 @@
 package handlers
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+	"os"
+
+	s "github.com/mellenooijen/gowebapp/structs"
+)
 
 func FileHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: Implement File Handler
+	filePath := "./public/direct/" + string(r.URL.Path[len("/direct/"):])
+	if _, err := os.Stat(filePath); errors.Is(err, os.ErrNotExist) {
+		ErrorHandler(w, r, s.IntError{Code: 404, Output: err.Error(), Page: filePath})
+	} else {
+		http.ServeFile(w, r, filePath)
+	}
 }
